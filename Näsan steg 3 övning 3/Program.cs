695ca12463c1ae7240579657a9ed470c3e718ffb
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2024-11-22 10:10:04 +0100

    klar

---
+++ Näsan steg 3 övning 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Ketchup
+{
+    static int Multi(int a, int b)
+    {
+        return a * b;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Multi(5, 3));
+        Console.ReadLine();
+    }
+
+
+}
